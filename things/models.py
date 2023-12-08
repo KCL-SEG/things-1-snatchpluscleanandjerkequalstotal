40b5b29423625dd241518f63684b36700e9936e7
Nassim Younes <k22043969@kcl.ac.uk>
2023-12-08 07:44:50 +0000

    1.1 and 1.2

--- things/models.py
+++ things/models.py
@@ -1,4 +1,5 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.exceptions import ValidationError
 from django.db import models
 
 # Create your models here.
